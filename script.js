1220d790eba9c75be25b9f1c5ad069bb74651673
chhhhaaaahhhh <charlotte.pochez@epita.fr>
2025-05-08 19:52:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("yes").addEventListener("change", function() {
+    if (this.checked) {
+        document.body.innerHTML = "<h1 class='texxt'><img src='pic_2.png' class='image'><br>yepii yepiii yepi</h1>";
+    }
+});
